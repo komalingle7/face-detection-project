589c1c44f33e436d0dedbbf9f689e4472a8729e0
komal R. ingle <136145537+komalingle7@users.noreply.github.com>
2024-11-20 19:21:46 +0530

    Add files via upload

---
+++ Face+Detection+App-+Code/Code/add_python_path.bat
@@ -0,0 +1,4 @@
+@echo off
+setx PATH "%PATH%;C:\Users\User\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts"
+echo Python Scripts path has been added to PATH
+pause
